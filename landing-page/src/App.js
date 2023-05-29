5483187ec5a5f9204d324c7d6a7ce388de6152cd
miha11-hash <d_mihaela@msn.com>
2023-05-29 15:14:51 +0300

    landing page setup files

---
+++ landing-page/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hero from './three/Hero';
+
+function App() {
+  return (
+    <div>
+      {/* navbar*/}
+      <Hero></Hero>
+      {/* projects*/}
+      {/* footer*/}
+    </div>
+  );
+}
+
+export default App;
