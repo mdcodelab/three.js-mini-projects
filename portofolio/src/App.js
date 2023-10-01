37dfb699b5a553a7000908c77e0e806a707c6a63
miha11-hash <d_mihaela@msn.com>
2023-10-01 11:05:42 +0300

    create scene, camera, renderer

--- portofolio/src/App.js
+++ portofolio/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import "./App.css";
+import * as THREE from "three";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const sceneRef = useRef(null);
+
+  useEffect(() => {
+    if (!sceneRef.current) return;
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGL1Renderer({
+      canvas: sceneRef.current,
+    });
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.setZ(30);
+    renderer.render(scene, camera);
+
+    
+  }, []);
+
+  return <canvas ref={sceneRef} id="canvas" />;
 }
 
 export default App;
